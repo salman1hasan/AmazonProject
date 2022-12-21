2f77347e916d4850a956e10ba5e36207816b4c06
Salman Hasan <96196317+salman1hasan@users.noreply.github.com>
2022-12-21 00:47:31 -0500

    Add files via upload

---
+++ pages/admin/orders.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import React from 'react';
+import { getError } from '../../utils/error';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true, error: '' };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, orders: action.payload, error: '' };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      state;
+  }
+}
+
+export default function AdminOrderScreen() {
+  const [{ loading, error, orders }, dispatch] = useReducer(reducer, {
+    loading: true,
+    orders: [],
+    error: '',
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await axios.get(`/api/admin/orders`);
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+      }
+    };
+    fetchData();
+  }, []);
+  return <Layout title="Admin Dashboard"></Layout>;
+  return <div>AdminOrderScreen</div>;
+}
+
+AdminOrderScreen.auth = { adminOnly: true };
