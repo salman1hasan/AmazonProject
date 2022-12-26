8a9f4f0d021ddd8a25ef3e485fe5d0edf6cfe404
Salman Hasan <96196317+salman1hasan@users.noreply.github.com>
2022-12-26 11:42:28 +0300

    Add files via upload

--- pages/admin/orders.js
+++ pages/admin/orders.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import React from 'react';
+import Link from 'next/link';
+import React, { useEffect, useReducer } from 'react';
+import Layout from '../../components/Layout';
 import { getError } from '../../utils/error';
 
 function reducer(state, action) {
@@ -34,8 +36,85 @@ export default function AdminOrderScreen() {
     };
     fetchData();
   }, []);
-  return <Layout title="Admin Dashboard"></Layout>;
-  return <div>AdminOrderScreen</div>;
+
+  return (
+    <Layout title="Admin Dashboard">
+      <div className="grid md:grid-cols-4 md:gap-5">
+        <div>
+          <ul>
+            <li>
+              <Link href="/admin/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link href="/admin/orders">
+                <a className="font-bold">Orders</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/admin/products">Products</Link>
+            </li>
+            <li>
+              <Link href="/admin/users">Users</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="overflow-x-auto md:col-span-3">
+          <h1 className="mb-4 text-xl">Admin Orders</h1>
+
+          {loading ? (
+            <div>Loading...</div>
+          ) : error ? (
+            <div className="alert-error">{error}</div>
+          ) : (
+            <div className="overflow-x-auto">
+              <table className="min-w-full">
+                <thead className="border-b">
+                  <tr>
+                    <th className="px-5 text-left">ID</th>
+                    <th className="p-5 text-left">USER</th>
+                    <th className="p-5 text-left">DATE</th>
+                    <th className="p-5 text-left">TOTAL</th>
+                    <th className="p-5 text-left">PAID</th>
+                    <th className="p-5 text-left">DELIVERED</th>
+                    <th className="p-5 text-left">ACTION</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {orders.map((order) => (
+                    <tr key={order._id} className="border-b">
+                      <td className="p-5">{order._id.substring(20, 24)}</td>
+                      <td className="p-5">
+                        {order.user ? order.user.name : 'DELETED USER'}
+                      </td>
+                      <td className="p-5">
+                        {order.createdAt.substring(0, 10)}
+                      </td>
+                      <td className="p-5">${order.totalPrice}</td>
+                      <td className="p-5">
+                        {order.isPaid
+                          ? `${order.paidAt.substring(0, 10)}`
+                          : 'not paid'}
+                      </td>
+                      <td className="p-5">
+                        {order.isDelivered
+                          ? `${order.deliveredAt.substring(0, 10)}`
+                          : 'not delivered'}
+                      </td>
+                      <td className="p-5">
+                        <Link href={`/order/${order._id}`} passHref>
+                          <a>Details</a>
+                        </Link>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 AdminOrderScreen.auth = { adminOnly: true };
